4e262560c297ad758781caad046f608b889cf8ea
Nicolas Gomez Marigliano <rodnicgm@gmail.com>
2022-09-09 02:31:14 -0300

    agregue borrar curso y su rspectivo sweet alert

--- js/admin.js
+++ js/admin.js
@@ -36,10 +36,10 @@ function crearFila (itemCurso){
     <td>${itemCurso.imagen}</td>
     <td>${itemCurso.descripcion}</td>
     <td>
-      <button class="btn btn-warning" onclick="editarPelicula('${itemCurso.codigo}')">
+      <button class="btn btn-warning" onclick="editarCurso('${itemCurso.codigo}')">
         <i class="bi bi-pencil-square"></i>
       </button>
-      <button class="btn btn-danger" onclick="borrarPelicula('${itemCurso.codigo}')">
+      <button class="btn btn-danger" onclick="borrarCurso('${itemCurso.codigo}')">
         <i class="bi bi-x-square"></i>
       </button>
     </td>
@@ -86,15 +86,54 @@ function crearCurso(e){
 
 function limpiarFormulario(){
     formCurso.reset();
-    //resetear la clases de bootstrap form-control
-    titulo.className = "form-control";
-    descripcion.className = "form-control";
-    imagen.className = "form-control";
-    genero.className = "form-control";
     codigo.className = "form-control";
+    nombre.className = "form-control";
+    precio.className = "form-control";
+    categoria.className = "form-control";
+    imagen.className = "form-control";
+    descripcion.className = "form-control";
+    
 }
-}
+
 
 function guardarDatosEnLS(){
     localStorage.setItem("listaCursosKey", JSON.stringify(listaCursos))
 }
+
+
+
+window.borrarCurso = function (codigo) {
+    Swal.fire({
+      title: "Eliminar Curso",
+      text: "Esta por eliminar el curso seleccionado",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      
+      if (result.isConfirmed) {
+        let copiaListaCursos = listaCursos.filter(
+          (cursos) => cursos.codigo != codigo
+        ); 
+        listaCursos = copiaListaCursos;
+        guardarDatosEnLS();
+        actualizarTabla();
+        Swal.fire(
+          "Curso eliminado",
+          "El curso seleccionado se borro correctamente",
+          "success"
+        );
+      }
+    });
+  };
+
+
+  function actualizarTabla() {
+    let tablaCurso = document.querySelector("#tablaCurso");
+    tablaCurso.innerHTML = "";
+    cargaInicial();
+  }
+  
