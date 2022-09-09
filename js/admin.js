bb05d03e4d529aa66d7230d1fa07b0ee60c29321
Nicolas Gomez Marigliano <rodnicgm@gmail.com>
2022-09-09 00:11:05 -0300

    Create terminado

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,6 @@
 import Curso from "./classCurso.js";
 
-let listaCursos = [];
+let listaCursos = JSON.parse(localStorage.getItem("listaCursosKey")) || [];
 
 const modalFormCurso = new bootstrap.Modal(document.querySelector("#modalCurso"));
 const btnCrearCurso = document.querySelector("#btnCrearCurso");
@@ -13,11 +13,48 @@ let descripcion = document.querySelector("#descripcion")
 let formCurso = document.querySelector("#formCurso")
 
 
-//agregar los eventos.
+
 btnCrearCurso.addEventListener("click", mostrarFormulario);
+
+formCurso.addEventListener("submit", crearCurso)
 
 function mostrarFormulario(){
     modalFormCurso.show();
     codigo.value = uuidv4()
     }
 
+function crearCurso(e){
+    e.preventDefault();
+    //AGREGAR VALIDACIONES CAMI.
+    
+    //CREAR CURSO
+    
+    const nuevoCurso = new Curso(codigo.value, nombre.value, precio.value, categoria.value, imagen.value, descripcion.value)
+   
+    console.log(nuevoCurso);
+    
+    //GUARDAR CURSO EN EL ARREGLO
+   
+    listaCursos.push(nuevoCurso);
+   
+    //guardar los datos en local storage
+   
+    guardarDatosEnLS()
+   
+    // LIMPIAR CURSO
+  
+    limpiarFormulario();
+  
+    //CERRAR LA VENTANA MODAL
+   
+    modalFormCurso.hide();
+}
+
+function limpiarFormulario(){
+    formCurso.reset();
+    //resetear la claser de bootstrap form-control
+}
+
+function guardarDatosEnLS(){
+    localStorage.setItem("listaCursosKey", JSON.stringify(listaCursos))
+}
