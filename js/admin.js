aedd6207eed5ce2fa6770ece2f5e14b8b99113ad
Nicolas Gomez Marigliano <rodnicgm@gmail.com>
2022-09-09 01:51:21 -0300

    Read en progreso, tablas dibujandose

--- js/admin.js
+++ js/admin.js
@@ -18,6 +18,35 @@ btnCrearCurso.addEventListener("click", mostrarFormulario);
 
 formCurso.addEventListener("submit", crearCurso)
 
+cargaInicial();
+
+function cargaInicial(){
+    if (listaCursos.length > 0) {
+        listaCursos.map((curso)=>{crearFila(curso)})
+    }
+}
+
+function crearFila (itemCurso){
+    let tablaCurso = document.querySelector("#tablaCurso");
+    tablaCurso.innerHTML += `<tr>
+    <th scope="row">${itemCurso.codigo}</th>
+    <td>${itemCurso.nombre}</td>
+    <td>${itemCurso.precio}</td>
+    <td>${itemCurso.categoria}</td>
+    <td>${itemCurso.imagen}</td>
+    <td>${itemCurso.descripcion}</td>
+    <td>
+      <button class="btn btn-warning" onclick="editarPelicula('${itemCurso.codigo}')">
+        <i class="bi bi-pencil-square"></i>
+      </button>
+      <button class="btn btn-danger" onclick="borrarPelicula('${itemCurso.codigo}')">
+        <i class="bi bi-x-square"></i>
+      </button>
+    </td>
+  </tr>`
+
+}
+
 function mostrarFormulario(){
     modalFormCurso.show();
     codigo.value = uuidv4()
