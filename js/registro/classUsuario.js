7355d15869c0e0a144032685345e649dc6f030b6
matias <pagosptcmatias@gmail.com>
2022-09-13 11:16:24 -0300

    quite archivos de registro

--- js/registro/classUsuario.js
+++
@@ -1,8 +0,0 @@
-export default class Persona {
-  constructor(nombre, contrasena) {
-    this.nombre = nombre;
-    this.contrasena = contrasena;
-  }
-
-  //   definir sett y get por is se necesita
-}
